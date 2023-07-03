e7fd99e01fa2af4c2b5ef5885a7087d27afe00a4
RmMetsch <130143544+RmMetsch@users.noreply.github.com>
2023-07-03 12:54:02 +0200

    Add files via upload

--- Main.jl
+++ Main.jl
@@ -1,57 +1,93 @@
 # import packages
-using DifferentialEquations, Plots, LinearAlgebra, Symbolics,SparseArrays,BenchmarkTools
-# import files
+using DifferentialEquations, Plots, LinearAlgebra, Symbolics,SparseArrays,BenchmarkTools, Sundials
+# import files 
 using .Matrices
-
 
 # geometry
 x_begin =  0
 x_end   =  1
 xrange  = x_end - x_begin
-nodes = 11
-dx = xrange/(nodes-1)
-x = x_begin:dx:x_end
+N  = 100
+dx = xrange/(N-1)
+x  = x_begin:dx:x_end
+
+# constants
+p = 1
+r = 1*dx    #factor is in [ohm/m], imes dx gives [ohm]
+
+#  time independent linear Matrix
+# submatrices
+Mkvl = Matrices.BuildMkvl(N) 
+Mkcl = Matrices.BuildMkcl(N)
+G    = zeros(N,N)
+# G    = Matrices.BuildConductanceMatrix(N)
+R    = Matrices.BuildResistanceMatrix(N)
+
+# Thermal/electrical linear matrix
+linThermMat = sparse(0.001.*Matrices.BuildHeatTransMatrix(N,dx))
+LinElecMat  = sparse(hcat(vcat(G,Mkvl),vcat(Mkcl,R)))
+
+# full Linear Matrix
+linMatrix   = blockdiag(LinElecMat,linThermMat)
+linMatrix   = linMatrix[2:end, 2:end]
+
+# Mass Matrix
+MassThermal    = sparse(Matrices.BuildThermalMassMatrix(N))
+MassElectrical = sparse(zeros(2N-1,2N-1)) 
+
+MassMatrix = blockdiag(MassElectrical,MassThermal) 
+MassMatrix = MassMatrix[2:end, 2:end]
+
+# # joule conduction matrix
+a = [0;1/2*ones(N-2)]
+JouleHeatFlowMatrix = spdiagm(N,N-1,0=>a,-1=>reverse(a) )
 
 
-#  Matrices
-M = Diagonal(ones(nodes))
-K = buildHeatTransMat()
+# external sources
+ext = [zeros(N-2); -1; zeros(2*N-1)] 
+
+
 
 # time
 tspan = (0.0,1)
 
-# constants
-p = 1
-
-# inital conditon
-T0 = ones(nodes)
-
 # #Define the problem
-function HeatEq(du, u, p, t)
+function HeatEq!(res,du, u, p, t)
+    ### unpack the solution vector
+    # V = u[1:N]
+    # T = u[2*N:end]
+    I = u[N:2*N-2]
 
-
-    du[:] = K*u 
-    du[1] = 0
-    du[end] = 0
-
-
+    ### add all contributions to the residual
+    # #linear contribution
+    res[:]  = linMatrix*u .+ ext
+    # time rate of change
+    res[:] += -MassMatrix*du
+    # nonlinear contribution'
+    res[:] += [zeros(N-1);zeros(N-1); r.*JouleHeatFlowMatrix*(I.^2)]
+    
 end
 
+# setting intial condition
+u0Elec  = vcat(collect(LinRange(0,-1,N)),ones(N-1))
+u0Elec  = u0Elec[2:end]
+u0Therm = [0.25-(x-0.5)^2 for x in x] 
 
-# # defining Jaconian
-du0 = copy(T0)
-u0  = copy(T0)
+u0 = vcat(u0Elec,u0Therm)
+du0 = [zeros(length(u0Elec));ones(length(u0Therm))]
+
+# solving the problem
+difvars = Bool[zeros(N-1);zeros(N-1);[0;ones(N-2);0]]
+
+prob = DAEProblem(HeatEq!, du0, u0, tspan, p, differential_vars = difvars) 
+sol = solve(prob, IDA())
 
 
-jac_sparsity = Symbolics.jacobian_sparsity((du,u) -> HeatEq(du, u, p, 0.0), du0, u0)
-
-# problem with jacobian
-f   = ODEFunction(HeatEq,
-                  jac_prototype = float.(jac_sparsity),
-                  mass_matrix = M) 
-
-
-prob_jacobian  = ODEProblem(f,T0,tspan,p,)
-sol = solve(prob_jacobian, TRBDF2())
 
 plot(sol.u)
+
+
+
+
+# jac_sparsity = Symbolics.jacobian_sparsity((du,u) -> HeatEq!(1, du, u, p, 0.0), du0, u0)
+# # problem with jacobian
--- Matrices.jl
+++ Matrices.jl
@@ -2,12 +2,11 @@ module Matrices
 using LinearAlgebra
 
 
-
 # Heat transfer matrix
-function buildHeatTransMat(nodes,dx)
+function BuildHeatTransMatrix(N::Int64,dx::Float64)
 
     # make base heattransfer matrix
-    k_1 = ones(nodes-1)
+    k_1 = ones(N-1)
     K = diagm(1 => k_1)
     K += K'
     K = K./dx^2
@@ -15,12 +14,32 @@ function buildHeatTransMat(nodes,dx)
     for i in 1: 1 : length(K[:,1])
         K[i,i] = -sum(K[i,:])
     end
-
+    
+    K[1,:] .= 0
+    K[end,:] .= 0
+    
     return K
 end
 
+function BuildConductanceMatrix(N::Int64)
+    g0 = N-1
+    G = diagm(1=> -g0*ones(N-1))
+    G += G'
+    G += Diagonal([g0; 2*g0*ones(N-2);g0])
+    return G
+end
 
-function buildMkcl(N)
+function BuildResistanceMatrix(N::Int64)
+    R = Diagonal(ones(N-1)/(N-1))
+    return R
+end
+
+function BuildThermalMassMatrix(N)
+    return Diagonal([0; ones(N-2) ;0])
+end
+
+
+function BuildMkcl(N)
     Mkcl = zeros(N,N-1)
     # Mkcl[1:end,1:end] .= 1
     for i in 1:1:N-1
@@ -33,7 +52,8 @@ end
 
 
 function BuildMkvl(N)
-    return buildMkcl(N)'
+    return BuildMkcl(N)'
 end
+
 
 end
